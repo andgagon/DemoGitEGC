7af0746f12f460c7049e1927f8d12cde30dfe7df
Andres <andgarq@hotmail.com>
2017-02-01 23:32:34 +0100

    Test añadidos

---
+++ src/test/TestFuncionesMatematicas.java
@@ -0,0 +1,20 @@
+import org.junit.*;
+import org.junit.Assert.*;
+import java.lang.IllegalArgumentException;
+
+public class testtt {
+
+	@Test
+	public void testSuma(){
+		Integer res = FuncionesMatematicas.suma(3, 3);
+		
+		Assert.assertTrue(res==6);
+	}
+	
+	@Test void testResta(){
+		Integer res = FuncionesMatematicas.resta(3, 3);
+		
+		Assert.assertTrue(res==0);
+	}
+	
+}

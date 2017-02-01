ac16d18c7768c759617995d56f37a887d3d1ce3b
Andres <andgarq@hotmail.com>
2017-02-01 23:26:35 +0100

    Añadida clase funciones matematicas

---
+++ src/main/FuncionesMatematicas.java
@@ -0,0 +1,13 @@
+package main;
+
+public class FuncionesMatematicas {
+
+	public Integer suma(Integer a, Integer b){
+		return a+b;
+	}
+	
+	public Integer resta(Integer a, Integer b){
+		return a-b;
+	}
+	
+}

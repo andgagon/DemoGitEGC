ec1b0894be90d7d1a42d4f00bc657197dc07b613
Andres <andgarq@hotmail.com>
2017-02-02 01:03:08 +0100

    Commit conflict2

--- src/main/FuncionesMatematicas.java
+++ src/main/FuncionesMatematicas.java
@@ -2,8 +2,8 @@ package main;
 
 public class FuncionesMatematicas {
 
-	public Integer suma(Integer a, Integer b){
-		return a+b;
+	public Integer suma(Integer  c, Integer b){
+		return c+b;
 	}
 	
 	public Integer resta(Integer a, Integer b){
